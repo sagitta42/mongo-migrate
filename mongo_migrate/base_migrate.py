f487b91b2dc4238c1b9d9b92ded60a00f99810f5
sagitta42 <mariia.redchuk@gmail.com>
2026-08-18 18:10:27 +0200

    add timestamps to document util method in base migration

--- mongo_migrate/base_migrate.py
+++ mongo_migrate/base_migrate.py
@@ -11,6 +11,8 @@
     Created on: 15-08-2023
     
 """
+from datetime import datetime
+
 import pymongo
 from abc import abstractmethod
 
@@ -45,3 +47,18 @@ class BaseMigration(object):
         """Implement this in the child classes"""
         pass
 
+    def add_timestamps(self, doc: dict, created: bool) -> dict:
+        """
+        Add created/updated at timestamps to given document.
+        """
+        ret = doc.copy()
+
+        now = datetime.now()
+        time_keys = ["updated"]
+        if created:
+            time_keys.append("created")
+
+        for key in time_keys:
+            ret[f"{key}_at"] = now
+
+        return ret
